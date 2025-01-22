43742895de3a16c160cd424d25563450ae154455
Ayobami <oniayobami100@gmail.com>
2025-01-22 17:21:27 +0100

    Added Authentication, login and register features

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dtos/user.dto';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dtos/auth.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) {}
+
+  @Post('register')
+  async register(@Body() dto: CreateUserDto) {
+    return await this.userService.create(dto);
+  }
+
+  @Post('login')
+  async login(@Body() dto: LoginDto) {
+    return await this.authService.login(dto);
+  }
+}
