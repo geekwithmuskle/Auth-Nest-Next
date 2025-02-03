edd3b7472495ca33ebd13b4681c74fc2877ef5f5
Ayobami <oniayobami100@gmail.com>
2025-02-03 09:57:59 +0100

    Added JWT guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { JwtGaurd } from 'src/auth/guards/jwt.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @UseGuards(JwtGaurd)
   @Get(':id')
   async getUserId(@Param('id') id: number) {
     return await this.userService.findById(id);
